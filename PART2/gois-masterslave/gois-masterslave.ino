54800328a9dba13bfb07f055c9bb6b5bd951b674
Diogogois96 <diogo.gois96@gmail.com>
2017-11-17 15:27:57 +0000

    final.ou nao

--- PART2/gois-masterslave/gois-masterslave.ino
+++ PART2/gois-masterslave/gois-masterslave.ino
@@ -12,9 +12,17 @@
 
 #include <Wire.h>
 
+#define analogPin 1
+
+//Resistencia do circuito do LDR
+#define r1 10000.0
+
+int ledpin = 9;
 
 int pin_verif = 2;
 int address, address_aux;
+long  temp;
+bool pronto = false;
 
 void setup() {
 
@@ -29,35 +37,111 @@ void setup() {
     address = 1;
     address_aux = 2;
   }
+
+  
   
   Wire.begin(address); // join i2c bus (address optional for master)
   Wire.onReceive(receiveEvent); // register event
   Serial.begin(9600);           // start serial for output
+  
 }
 
 
-
+void inicio()
+{
+  //calibrar();
+  Serial.print("ola");
+}
   
 void loop() {
   Wire.beginTransmission(address_aux); // transmit to device #8
-  Wire.write("x is ");        // sends five bytes
+ Wire.write("x is ");        // sends five bytes
   Wire.write(address);              // sends one byte
   Wire.endTransmission();    // stop transmitting
 
   
-  delay(500);
+  delay(100);
 
+  if(millis() - temp > 300)
+  {
+    pronto = false;
+  }
+  Serial.print(pronto);
   
 }
 
+//função que calcula o valor de luxs
+float calc_luxs(int val)
+{
+  float R2;
+  float luxs;
+  float tensao;
+  
+  //converte de 0-1023 para 0-5V
+  tensao = (5 * val) / 1023.0;
+
+  //equacao do divisor de tensao para obter resistencia R2
+  R2 = ((r1 * 5) / tensao) - r1;
+
+  //uso de reta logaritmica para converter de R para lux
+  luxs = ((log10(R2) - 4.8451) / -0.7186);
+  luxs = pow(10, luxs);
+
+  return luxs;
+}
+
+//verifica se arduino nao esta a mandar muita informação há algum tempo
+
+
+//função que calibra ambos os leds dos arduinos
+void calibrar()
+{
+  if(address == 1)
+  {
+    digitalWrite(ledpin, HIGH);
+  }
+  else
+  {
+    digitalWrite(ledpin, LOW);
+  }
+    
+  delay(250);
+
+  Serial.println(calc_luxs(analogRead(analogPin)));
+  
+  if(address == 2)
+  {
+    digitalWrite(ledpin, HIGH);
+  }
+  else
+  {
+    digitalWrite(ledpin, LOW);
+  }
+    
+  Serial.println(calc_luxs(analogRead(analogPin)));
+  
+  
+  
+  
+  
+}
 
 // function that executes whenever data is received from master
 // this function is registered as an event, see setup()
 void receiveEvent(int howMany) {
+  bool pronto_antes = pronto;
   while (1 < Wire.available()) { // loop through all but the last
     char c = Wire.read(); // receive byte as a character
     Serial.print(c);         // print the character
+    pronto = true;
+    temp = millis();
   }
+
+  if(pronto_antes == false && pronto == true)
+  {
+    inicio();
+  }
+  
   int x = Wire.read();    // receive byte as an integer
   Serial.println(x);         // print the integer
 }
